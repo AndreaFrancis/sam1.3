cebbc5c240d01b2d213b83595c58d3e1572b8dba
= <=>
2015-07-19 19:04:28 -0400

    Se completo el modulo de creacion de Pruebas

--- client/exams/exams.js
+++ client/exams/exams.js
@@ -17,12 +17,43 @@ angular.module("sam-1").controller("ExamsListCtrl",['$scope','$meteor','ModalSer
     }]);
 
 function AddExamCtrl($scope, $meteor, notificationService, $mdDialog) {
-
+    $scope.typeEvaluations = $meteor.collection(TypeEvaluation);
     $scope.measures = $meteor.collection(Measures);
     $scope.exams = $meteor.collection(Exams);
     $scope.newExam = {};
+    $scope.targets = new Array();
+    $scope.evaluations = new Array();
+    $scope.typeEvaluation = {};
+    $scope.newTarget = {};
+    $scope.newEvaluation = {};
+    $scope.fields = [];
+
+
+    $scope.saveEvaluation = function() {
+        var evaluation = {
+            name: $scope.newEvaluation.name,
+            type : $scope.typeEvaluation._id
+        }
+        for(var i= 0; i<$scope.typeEvaluation.fields.length; i++) {
+            var fieldName = $scope.typeEvaluation.fields[i];
+            evaluation[fieldName] = $scope.fields[i];
+        }
+        $scope.evaluations.push(evaluation);
+        $scope.newEvaluation = {};
+        $scope.fields = [];
+    }
+
+    $scope.saveTarget = function() {
+        $scope.newTarget.evaluations = $scope.evaluations;
+        $scope.targets.push($scope.newTarget);
+        $scope.newTarget = '';
+        $scope.evaluations = [];
+    }
 
     $scope.save = function() {
+        var targetsToJson = angular.toJson($scope.targets);
+        var targetsToArray = JSON.parse(targetsToJson);
+        $scope.newExam.targets = targetsToArray;
         $scope.exams.save($scope.newExam).then(
             function(number){
                 notificationService.showSuccess("Se ha registrado correctamente la prueba");
---
+++ model/typeEvaluation.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Andrea on 19/07/2015.
+ */
+
+TypeEvaluation = new Mongo.Collection("TypeEvaluation");
+Ground.Collection(TypeEvaluation,'typeEvaluation');
